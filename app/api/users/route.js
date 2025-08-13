cce106916b2eeb88437ce51bcbae78ba05828b95
FumoRin <edu.dimasfaizsatria@gmail.com>
2025-08-13 16:14:58 +0700

    feature: Swagger API Documentation setup

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,28 +1,53 @@
 import prisma from "@/lib/prisma";
 
+/**
+ * @swagger
+ * /api/users:
+ *   get:
+ *     summary: Get all users
+ *     description: Returns a list of all users
+ *     responses:
+ *       200:
+ *         description: A list of users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   name:
+ *                     type: string
+ *                   email:
+ *                     type: string
+ */
+
 export async function GET(request) {
-    try {
-        const users = await prisma.users.findMany({
-            select: {
-                name: true,
-                email: true,
-                role: true
-            }
-        });
-        return new Response(JSON.stringify(users), {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-    } catch (error) {
-        console.error("Error fetching users:", error);
-        return new Response(JSON.stringify({
-            error: "Internal Server Error"
-        }), {
-            status: 500,
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-    }
+  try {
+    const users = await prisma.users.findMany({
+      select: {
+        name: true,
+        email: true,
+        role: true,
+      },
+    });
+    return new Response(JSON.stringify(users), {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    return new Response(
+      JSON.stringify({
+        error: "Internal Server Error",
+      }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
 }
