c398c0154abedcb273e600c26ca58927fb9c01b5
Muhammad Syarif <syarkev@gmail.com>
2025-08-27 08:33:16 +0700

    update sweager

--- app/api/soldier/data/getAll/route.js
+++ app/api/soldier/data/getAll/route.js
@@ -1,5 +1,71 @@
 import prisma from "@/lib/prisma";
 
+/**
+ * @swagger
+ * /api/soldier/data/getAll:
+ *   get:
+ *     summary: Ambil daftar personil
+ *     description: Mengambil data personil dengan pagination,ini endpoinnya "/api/soldier/data/getAll?page=1&limit=50".
+ *     tags:
+ *       - Soldier
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *         required: false
+ *         description: Nomor halaman (default 1)
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *           maximum: 100
+ *         required: false
+ *         description: Jumlah data per halaman (default 50, maksimal 100)
+ *     responses:
+ *       200:
+ *         description: Data personil berhasil diambil
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   description: Daftar personil
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         example: 1
+ *                       NRP:
+ *                         type: string
+ *                         example: "123456"
+ *                       NAMA:
+ *                         type: string
+ *                         example: "Ferdi"
+ *                       PANGKAT:
+ *                         type: string
+ *                         example: "Brigjen"
+ *                       KESATUAN:
+ *                         type: string
+ *                         example: "Kodam III"
+ *                 total:
+ *                   type: integer
+ *                   example: 120
+ *                 page:
+ *                   type: integer
+ *                   example: 1
+ *                 totalPages:
+ *                   type: integer
+ *                   example: 3
+ *       500:
+ *         description: Terjadi kesalahan server
+ */
+
 export async function GET(request) {
     try {
         const { searchParams } = new URL(request.url);
--- app/api/soldier/data/route.js
+++ app/api/soldier/data/route.js
@@ -1,5 +1,101 @@
 import prisma from "@/lib/prisma";
 
+/**
+ * @swagger
+ * /api/data/soldier/data:
+ *   get:
+ *     summary: Ambil daftar personil
+ *     description: Mengambil data personil dengan filter opsional dan pagination, ini endpoinnya "/api/data/soldier/data?page=1&limit=10&nama=Ferdi&pangkat=Brigjen" nah untuk nrp, nama, pangkat, kesatuan itu opsional
+ *     tags:
+ *       - Soldier
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *         required: false
+ *         description: Halaman data (default 1)
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *           maximum: 100
+ *         required: false
+ *         description: Jumlah data per halaman (default 50, maksimal 100)
+ *       - in: query
+ *         name: nrp
+ *         schema:
+ *           type: string
+ *         required: false
+ *         description: Cari berdasarkan NRP
+ *       - in: query
+ *         name: nama
+ *         schema:
+ *           type: string
+ *         required: false
+ *         description: Cari berdasarkan nama
+ *       - in: query
+ *         name: pangkat
+ *         schema:
+ *           type: string
+ *         required: false
+ *         description: Cari berdasarkan pangkat
+ *       - in: query
+ *         name: kesatuan
+ *         schema:
+ *           type: string
+ *         required: false
+ *         description: Cari berdasarkan kesatuan
+ *     responses:
+ *       200:
+ *         description: Data personil berhasil diambil
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         example: 1
+ *                       NRP:
+ *                         type: string
+ *                         example: "123456"
+ *                       NAMA:
+ *                         type: string
+ *                         example: "Ferdi"
+ *                       PANGKAT:
+ *                         type: string
+ *                         example: "Brigjen"
+ *                       KESATUAN:
+ *                         type: string
+ *                         example: "Kodam III"
+ *                 page:
+ *                   type: integer
+ *                   example: 1
+ *                 limit:
+ *                   type: integer
+ *                   example: 50
+ *                 total:
+ *                   type: integer
+ *                   example: 100
+ *                 totalPages:
+ *                   type: integer
+ *                   example: 2
+ *       400:
+ *         description: Parameter tidak valid
+ *       404:
+ *         description: Data tidak ditemukan
+ *       500:
+ *         description: Terjadi kesalahan server
+ */
+
 export async function GET(req) {
   try {
     const { searchParams } = new URL(req.url);
